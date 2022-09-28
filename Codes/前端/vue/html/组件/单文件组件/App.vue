fb61049dd34e4c3e41c63f875876b36c42260398
cjshipit <1390256375@qq.com>
2022-09-28 14:34:50 +0800

    vue-脚手架

---
+++ Codes/前端/vue/html/组件/单文件组件/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <school></school>
+    <br/>
+    <student></student>
+  </div>
+</template>
+
+<script>
+    import school from './school.vue'
+    import student from './student.vue'
+    export default {
+        name:'App',
+        components:{
+            school:school,
+            student:student,
+        }
+
+    }
+</script>
+
+<style>
+
+</style>
---
+++ Codes/前端/vue/html/组件/单文件组件/school.vue
@@ -0,0 +1,24 @@
+
+<template>
+<!--写结构-->
+<div>
+    <span>{{name}}</span>
+    <span>{{address}}</span>
+</div>    
+</template>
+<script>
+//写交互
+export default {
+    name:'school',
+    data() {
+        return {
+            name:'just',
+            address:"zhenjiang"
+        }
+    },
+}
+</script>
+
+<style>
+/*写样式*/
+</style>
